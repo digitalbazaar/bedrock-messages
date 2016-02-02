6054512c13c7bfa525dcbaeb3284aeeec9597b2a
Alex Lamar <alamar@digitalbazaar.com>
2016-02-02 17:19:05 -0500

    Add new tests, expose async for testing.

--- tests/05-batching.js
+++ tests/05-batching.js
@@ -21,7 +21,7 @@ var uuid = require('node-uuid').v4;
 var store = database.collections.messages;
 var storeBatch = database.collections.messagesBatch;
 
-describe('bedrock-messages message batching functions', function() {
+describe.only('bedrock-messages message batching functions', function() {
   before(function(done) {
     helpers.prepareDatabase(mockData, done);
   });
@@ -142,6 +142,98 @@ describe('bedrock-messages message batching functions', function() {
         }]
       }, done);
     });
+    it('does nothing when batch is "delivered" first by another process', function(done) {
+      var message = util.clone(mockData.messages.alpha);
+      var batch = util.clone(mockData.batches.alpha);
+      batch.value.dirty = true;
+      batch.value.messages[message.value.id] = true;
+      async.auto({
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        otherProcessUpdate: ['insertMessage', 'insertBatch', function(callback) {
+          // Other process increments batch ID
+          var q = {id: database.hash(batch.value.recipient)};
+          var u = {$inc: {'value.id': 1}};
+          storeBatch.update(q, u, callback);
+        }],
+        act: ['otherProcessUpdate', function(callback) {
+          brMessages._batchMessage(batch.value, message.value, callback);
+        }],
+        messageQuery: ['act', function(callback) {
+          store.findOne({}, callback);
+        }],
+        batchQuery: ['act', function(callback) {
+          storeBatch.findOne({}, callback);
+        }],
+        test: ['messageQuery', 'batchQuery', function(callback, results) {
+          var m = results.messageQuery.value;
+          m.meta.batch.id.should.equal(message.value.meta.batch.id);
+          m.meta.batch.state.should.equal(message.value.meta.batch.state);
+          var b = results.batchQuery.value;
+          b.id.should.equal(batch.value.id + 1);
+          b.recipient.should.equal(message.value.recipient);
+          should.exist(b.messages);
+          b.messages.should.be.an('object');
+          should.exist(b.messages[message.value.id]);
+          callback();
+        }]
+      }, done);
+    });
+    it('message goes to "ready" state and message stays in join list if batch increments mid-function', 
+      function(done) {
+      var message = util.clone(mockData.messages.alpha);
+      var batch = util.clone(mockData.batches.alpha);
+      batch.value.dirty = true;
+      batch.value.messages[message.value.id] = true;
+      async.auto({
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        updateBatch: ['insertMessage', 'insertBatch', function(callback) {
+          brMessages
+            ._batchMessagePhase1(batch.value, message.value, null, callback);
+        }],
+        updateMessage: ['updateBatch', function(callback, results) {
+          brMessages
+            ._batchMessagePhase2(batch.value, message.value, null, callback, {'updateBatch': results.updateBatch});
+        }],
+        otherProcessUpdate: ['updateMessage', function(callback) {
+          var q = {id: database.hash(batch.value.recipient)};
+          var u = {$inc: {'value.id': 1}};
+          storeBatch.update(q, u, callback);
+        }],
+        removeFromMap: ['otherProcessUpdate', function(callback, results) {
+          brMessages
+          ._batchMessagePhase3(batch.value, message.value, null, callback, {'updateMessage': results.updateMessage});
+        }],
+        messageQuery: ['removeFromMap', function(callback) {
+          store.findOne({}, callback);
+        }],
+        batchQuery: ['removeFromMap', function(callback) {
+          storeBatch.findOne({}, callback);
+        }],
+        test: ['messageQuery', 'batchQuery', function(callback, results) {
+          var m = results.messageQuery.value;
+          m.meta.batch.id.should.equal(message.value.meta.batch.id);
+          m.meta.batch.state.should.equal('ready');
+          var b = results.batchQuery.value;
+          b.id.should.equal(batch.value.id + 1);
+          b.recipient.should.equal(message.value.recipient);
+          b.dirty.should.equal(true);
+          should.exist(b.messages);
+          b.messages.should.be.an('object');
+          should.exist(b.messages[message.value.id]);
+          callback();
+        }]
+      }, done);
+    });
   }); // end batchMessage
   describe('getUnbatchedMessage function', function() {
     afterEach(function(done) {
@@ -253,6 +345,33 @@ describe('bedrock-messages message batching functions', function() {
         }]
       }, done);
     });
+    it('creates a batch if there is pending messages but no batch for recipient', function(done) {
+      var message = util.clone(mockData.messages.alpha);
+      message.value.meta.batch.state = 'pending';
+      async.auto({
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        getUnbatched: ['insertMessage', function(callback) {
+          brMessages._getUnbatchedMessage(null, callback);
+        }],
+        batchQuery: ['getUnbatched', function(callback) {
+          storeBatch.findOne({}, callback);
+        }],
+        test: ['batchQuery', function(callback, results) {
+          should.exist(results.getUnbatched);
+          should.exist(results.batchQuery);
+          //var b = results.batchQuery.value;
+          //b.id.should.equal(0);
+          //b.recipient.should.equal(message.value.recipient);
+          //should.exist(b.messages);
+          //b.messages.should.be.an('object');
+          //should.exist(b.messages[message.value.id]);
+
+          callback();
+        }]
+      }, done);
+    });
   }); // end getUnbatchedMessage
   describe('deliverBatch function', function() {
     afterEach(function(done) {
@@ -337,6 +456,56 @@ describe('bedrock-messages message batching functions', function() {
           should.exist(results.readBatch);
           results.readBatch.id.should.equal(0);
           should.not.exist(results.closeBatch);
+          callback();
+        }]
+      }, done);
+    });
+    it('test increment message id mid function', function(done) {
+      var batch = util.clone(mockData.batches.alpha);
+      var message = util.clone(mockData.messages.alpha);
+      message.value.meta.batch.state = 'ready';
+      async.auto({
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        readBatch: ['insertMessage', 'insertBatch', function(callback) {
+          brMessages
+            ._closeBatchPhase1(batch.value.recipient, null, callback);
+        }],
+        findMessage: ['readBatch', function(callback, results) {
+          brMessages
+            ._closeBatchPhase2(batch.value.recipient, null, callback, results);
+        }],
+        updateBatch: ['findMessage', function(callback, results) {
+          brMessages
+            ._closeBatchPhase3(batch.value.recipient, null, callback, results);
+        }],
+        otherProcessUpdate: ['updateBatch', function(callback) {
+          var u = {$inc: {'value.meta.batch.id': 1}};
+          store.update({}, u, callback);
+        }],
+        getMessages: ['otherProcessUpdate', function(callback, results) {
+          brMessages
+          ._closeBatchPhase4(batch.value.recipient, null, callback, results);
+        }],
+        messageQuery: ['getMessages', function(callback) {
+          store.findOne({}, callback);
+        }],
+        batchQuery: ['getMessages', function(callback) {
+          storeBatch.findOne({}, callback);
+        }],
+        test: ['messageQuery', 'batchQuery', function(callback, results) {
+          var m = results.messageQuery.value;
+          m.meta.batch.id.should.equal(message.value.meta.batch.id + 1);
+          m.meta.batch.state.should.equal('ready');
+          var b = results.batchQuery.value;
+          b.id.should.equal(batch.value.id + 1);
+          b.recipient.should.equal(message.value.recipient);
+          should.exist(b.messages);
+          b.messages.should.be.an('object');
           callback();
         }]
       }, done);
