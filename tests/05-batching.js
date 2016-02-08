4f55c97947370030314aec8f3f347a8d8a4a9540
Alex Lamar <alamar@digitalbazaar.com>
2016-02-08 17:27:04 -0500

    Add new tests, fix null reference bug.

--- tests/05-batching.js
+++ tests/05-batching.js
@@ -21,7 +21,7 @@ var uuid = require('node-uuid').v4;
 var store = database.collections.messages;
 var storeBatch = database.collections.messagesBatch;
 
-describe.only('bedrock-messages message batching functions', function() {
+describe('bedrock-messages message batching functions', function() {
   before(function(done) {
     helpers.prepareDatabase(mockData, done);
   });
@@ -372,6 +372,101 @@ describe.only('bedrock-messages message batching functions', function() {
         }]
       }, done);
     });
+    it('returns a reset message', function(done) {
+      // Message not in join map, message.meta.batch.id < batch.id
+      var batch = util.clone(mockData.batches.alpha);
+      var message = util.clone(mockData.messages.alpha);
+      message.value.meta.batch.state = 'pending';
+      message.value.meta.batch.id = 0;
+      batch.value.id = 1;
+      async.auto({
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        getUnbatched: ['insertMessage', 'insertBatch', function(callback) {
+          brMessages._getUnbatchedMessage(batch.value, callback);
+        }],
+        getMessage: ['getUnbatched', function(callback) {
+          store.findOne({}, callback);
+        }],
+        test: ['getMessage', function(callback, results) {
+          should.exist(results.getUnbatched);
+          results.getUnbatched.should.be.an('object');
+          should.exist(results.getUnbatched.batch);
+          results.getUnbatched.batch.should.be.an('object');
+          results.getUnbatched.batch.should.deep.equal(batch.value);
+          should.exist(results.getMessage.value);
+          results.getMessage.value.should.be.an('object');
+          results.getMessage.value.meta.batch.id.should.equal(1);
+          callback();
+        }]
+      }, done);
+    });
+    it('also returns a reset message', function(done) {
+      // Message in join map, message.meta.batch.id < batch.id
+      var batch = util.clone(mockData.batches.alpha);
+      var message = util.clone(mockData.messages.alpha);
+      message.value.meta.batch.state = 'pending';
+      message.value.meta.batch.id = 0;
+      batch.value.id = 1;
+      batch.value.messages = [message.value.id];
+      async.auto({
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        getUnbatched: ['insertMessage', 'insertBatch', function(callback) {
+          brMessages._getUnbatchedMessage(batch.value, callback);
+        }],
+        getMessage: ['getUnbatched', function(callback) {
+          store.findOne({}, callback);
+        }],
+        test: ['getMessage', function(callback, results) {
+          should.exist(results.getUnbatched);
+          results.getUnbatched.should.be.an('object');
+          should.exist(results.getUnbatched.batch);
+          results.getUnbatched.batch.should.be.an('object');
+          results.getUnbatched.batch.should.deep.equal(batch.value);
+          should.exist(results.getMessage.value);
+          results.getMessage.value.should.be.an('object');
+          results.getMessage.value.meta.batch.id.should.equal(1);
+          callback();
+        }]
+      }, done);
+    });
+    it('returns a reset batch', function(done) {
+      // Message pending, message.meta.batch.id == batch.id
+      var batch = util.clone(mockData.batches.alpha);
+      var message = util.clone(mockData.messages.alpha);
+      message.value.meta.batch.state = 'pending';
+      message.value.meta.batch.id = 1;
+      batch.value.id = 1;
+      async.auto({
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        getUnbatched: ['insertMessage', 'insertBatch', function(callback) {
+          brMessages._getUnbatchedMessage(batch.value, callback);
+        }],
+        getBatch: ['getUnbatched', function(callback) {
+          storeBatch.findOne({}, callback);
+        }],
+        test: ['getBatch', function(callback, results) {
+          should.exist(results.getBatch.value);
+          results.getBatch.value.should.be.an('object');
+          results.getBatch.value.dirty.should.equal(false);
+          callback();
+        }]
+      }, done);
+    });
   }); // end getUnbatchedMessage
   describe('deliverBatch function', function() {
     afterEach(function(done) {
@@ -460,7 +555,8 @@ describe.only('bedrock-messages message batching functions', function() {
         }]
       }, done);
     });
-    it('test increment message id mid function', function(done) {
+    it('does not advance state illegally when another process updates message batch id mid-function', 
+      function(done) {
       var batch = util.clone(mockData.batches.alpha);
       var message = util.clone(mockData.messages.alpha);
       message.value.meta.batch.state = 'ready';
@@ -503,6 +599,61 @@ describe.only('bedrock-messages message batching functions', function() {
           var m = results.messageQuery.value;
           m.meta.batch.id.should.equal(message.value.meta.batch.id + 1);
           m.meta.batch.state.should.equal('ready');
+          var b = results.batchQuery.value;
+          b.id.should.equal(batch.value.id + 1);
+          b.recipient.should.equal(message.value.recipient);
+          should.exist(b.messages);
+          b.messages.should.be.an('object');
+          callback();
+        }]
+      }, done);
+    });
+    it('does not change state when message batch incremennts mid function',
+      function(done) {
+      var batch = util.clone(mockData.batches.alpha);
+      var message = util.clone(mockData.messages.alpha);
+      message.value.meta.batch.state = 'ready';
+      async.auto({
+        insertBatch: function(callback) {
+          storeBatch.insert(batch, callback);
+        },
+        insertMessage: function(callback) {
+          store.insert(message, callback);
+        },
+        readBatch: ['insertMessage', 'insertBatch', function(callback) {
+          brMessages
+            ._closeBatchPhase1(batch.value.recipient, null, callback);
+        }],
+        otherProcessUpdate: ['readBatch', function(callback) {
+          var u = {$inc: {'value.id': 1}};
+          storeBatch.update({}, u, callback);
+        }],
+        findMessage: ['otherProcessUpdate', function(callback, results) {
+          brMessages
+            ._closeBatchPhase2(batch.value.recipient, null, callback, results);
+        }],
+        updateBatch: ['findMessage', function(callback, results) {
+          brMessages
+            ._closeBatchPhase3(batch.value.recipient, null, callback, results);
+        }],
+        getMessages: ['updateBatch', function(callback, results) {
+          brMessages
+          ._closeBatchPhase4(batch.value.recipient, function(err, results) {
+            // Should return here, with null err and results.
+            callback();
+          }, callback, results);
+        }],
+        messageQuery: ['getMessages', function(callback) {
+          store.findOne({}, callback);
+        }],
+        batchQuery: ['getMessages', function(callback) {
+          storeBatch.findOne({}, callback);
+        }],
+        test: ['messageQuery', 'batchQuery', function(callback, results) {
+          // No state should be updated other than the batch id write
+          var m = results.messageQuery.value;
+          m.meta.batch.id.should.equal(message.value.meta.batch.id);
+          m.meta.batch.state.should.equal(message.value.meta.batch.state);
           var b = results.batchQuery.value;
           b.id.should.equal(batch.value.id + 1);
           b.recipient.should.equal(message.value.recipient);
